2f0d4ba6f378e44f9ca8fd85be2ee0f581db0b36
Nikita Tsilin <nikita_tsilin@mail.ru>
2018-04-01 16:28:10 +0300

    *

--- kafka/src/main/java/com/application/model/User.java
+++ kafka/src/main/java/com/application/model/User.java
@@ -1,17 +1,15 @@
 package com.application.model;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.experimental.Accessors;
 
 @Data
-@AllArgsConstructor
 @Accessors(chain = true)
 public class User {
 
-    private String firstName;
+    private final String firstName;
 
-    private String lasName;
+    private final String lasName;
 
-    private long id;
+    private final long id;
 }
--- kafka/src/test/java/com/application/service/kafka/ServiceTest.java
+++ kafka/src/test/java/com/application/service/kafka/ServiceTest.java
@@ -42,7 +42,8 @@ public class ServiceTest {
     public void setUp() throws Exception {
         // wait until the partitions are assigned
         for (MessageListenerContainer messageListenerContainer : kafkaListenerEndpointRegistry.getListenerContainers()) {
-            ContainerTestUtils.waitForAssignment(messageListenerContainer,
+            ContainerTestUtils.waitForAssignment(
+                    messageListenerContainer,
                     kafkaEmbedded.getPartitionsPerTopic());
         }
     }
